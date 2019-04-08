49c90266521a91ca94191597893af9d8b598874e
Denmark Garcia <denmark@philsmile.com>
2019-04-08 19:51:55 +0800

    Dockerized application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel in Docker
+                    Laravel in Docker!
                 </div>
 
                 <div class="links">
